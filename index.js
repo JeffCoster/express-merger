bebe9ca5ffc318a8baeca99a038357c88e056221
Jeff Coster <122172619+JeffCoster@users.noreply.github.com>
2023-08-31 16:27:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+import express from "express"
+import {
+  compose 
+} from "../merger/merger-functions.js"
+import {
+   validateMergeMapToSchema
+} from "../merger/merger-map-validate.js"
+
+import jsdom from "jsdom"
+
+import {
+   dataSources
+} from "./examples/simpleProductList/content/data-sources.js"
+import {
+   mergerMap
+} from "./examples/simpleProductList/merger-map.js"
+
+import {
+   dataSources as dataSourcesLevels
+} from "./examples/levels/content/data-sources.js"
+import {
+   mergerMap as mergeMapLevels
+} from "./examples/levels/merger-map.js"
+
+
+import Ajv2019 from "ajv/dist/2019.js"
+
+global.debug = true;
+
+var mergeMap4View;
+var dataSources4View;
+
+var app = express();
+
+// const fs = require("fs") // this engine requires the fs module
+app.engine("html", (filePath, options, callback) => { // define the template engine
+   const {JSDOM} = jsdom;
+   var document;
+   var dom = JSDOM.fromFile(filePath, {
+      includeNodeLocations: true
+   }).then(dom => {
+      validateMergeMapToSchema(options.mergeMap4View, Ajv2019);
+      document = dom.window.document;
+      compose(options.mergeMap4View, options.dataSources4View, document);
+      return callback(null, dom.serialize());
+   });
+})
+
+app.set("views", "./examples") // specify the views directory
+//app.set("view engine", "merge") // register the template engine module
+
+app.get("/products", (req, res) => {
+   mergeMap4View = mergerMap;
+   dataSources4View = dataSources;   
+   res.render("simpleProductList/product-lister-template.html", {mergeMap4View, dataSources4View});
+})
+
+app.get("/taxonomy", (req, res) => {
+   mergeMap4View = mergeMapLevels;
+   dataSources4View = dataSourcesLevels;
+   res.render("levels/levels.html", {mergeMap4View, dataSources4View});
+})
+
+app.use(express.static("static"));
+
+app.listen(3000);
+
+console.log("Node listening on port 3000");
