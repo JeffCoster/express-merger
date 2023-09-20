7ae658b4dcb0137580adc3f73f62509ff4e8adec
Jeff Coster <122172619+JeffCoster@users.noreply.github.com>
2023-09-20 14:01:45 +0100

    tidy comments

--- examples/simpleProductList/content/data-sources.js
+++ examples/simpleProductList/content/data-sources.js
@@ -6,6 +6,8 @@ export const dataSources = {};
 dataSources.globals = globalContent;
 dataSources.productList = prods.products;
 
-// min and max index of source Products to show 
+// min (start index) and max to show of source Products
+// indexing starts at 0, and in example there are 5 products
+// So min of 2 and max to show of 3 means show 3rd,4th, and 5th
 dataSources.minProducts = 2;
 dataSources.maxProducts = 3;
